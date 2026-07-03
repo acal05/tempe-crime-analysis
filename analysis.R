51fa2dc4ae2a9ab1906e906196cf23cb50551565
Abigail Calvert <abigailcalvert@Abigails-MacBook-Air.local>
2026-07-03 15:36:07 -0400

    Complete seasonal arrests bar chart

--- analysis.R
+++ analysis.R
@@ -1,8 +1,7 @@
 #load the necessary packages
 library(tidyverse)
-#load package to make the arrest_dt easier to read
 library(lubridate)
-#getting column and row numbers, as well as chr and dlbs
+#import data
 crime_data <- read_csv("data/Temp_Agol_Arrests_Con_TypeA_OpenData.csv")
 #looking at the columns
 glimpse(crime_data)
@@ -54,8 +53,80 @@ ggsave(
 class(crime_data$arrest_dt)
 #Getting the exact format of the arrest_dt column
 head(crime_data$arrest_dt)
-#Converting the columns to make it easier to go through
+#Converting the arrest date columns to analyze
 crime_data_clean <- crime_data
 crime_data_clean$arrest_datetime <- ymd_hms(crime_data_clean$arrest_dt)
 class(crime_data_clean$arrest_datetime)
 head(crime_data_clean$arrest_datetime)
+# Feature Engineering
+# Create new variables from the arrest date and time to make 
+# future analysis easier.
+crime_data_clean <- crime_data_clean %>%
+  mutate(
+    arrest_year = year(arrest_datetime),
+    arrest_month = month(arrest_datetime),
+    arrest_month_name = month(arrest_datetime, label = TRUE, abbr = FALSE),
+    arrest_day = day(arrest_datetime),
+    arrest_weekday = wday(arrest_datetime, label = TRUE, abbr = FALSE),
+    arrest_hour = hour(arrest_datetime),
+    arrest_season = case_when(
+      arrest_month %in% c(12, 1, 2) ~ "Winter",
+      arrest_month %in% c(3,4,5) ~ "Spring",
+      arrest_month %in% c(6,7,8) ~ "Summer",
+      TRUE ~ "Fall"
+    )
+  )
+crime_data_clean
+glimpse(crime_data_clean)
+
+#Count arrests by seasons
+season_counts <- crime_data_clean %>%
+  count(arrest_season) %>%
+  mutate(
+    arrest_season = factor(
+      arrest_season,
+      levels = c("Winter", "Spring", "Summer", "Fall")
+    )
+  )
+season_counts
+#Creating a bar chart for seasonal arrests
+ggplot(season_counts, aes(x = arrest_season, y = n)) +
+  geom_col(
+    aes(fill = arrest_season),
+    color = "black") +
+  labs(
+    title = "Tempe's Total Arrests by Season",
+    x = "Season",
+    y = "Number of Arrests"
+  ) +
+  scale_fill_manual(
+    values = c(
+      "Winter" = "steelblue",
+      "Spring" = "forestgreen",
+      "Summer" = "gold",
+      "Fall" = "darkorange"
+    ) 
+  ) +
+  scale_y_continuous(
+    labels = scales::comma
+  )+
+  geom_text(
+    aes(label = scales::comma(n)),
+    vjust = -0.3
+    )+
+  theme_bw() +
+  theme (
+    plot.title = element_text(
+    hjust = 0.5,
+    size = 18,
+    face = "bold"
+    ),
+    legend.position = "none"
+  )
+#Saving seasonal arrests bar chart to plots
+ggsave(
+  filename = "plots/seasonal_arrests_bar_chart.png",
+  width = 10,
+  height = 6,
+  dpi = 300,
+)
